9d44e013fdfb3b5b277c138083cf79de14de75af
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-08-24 23:36:42 +0530

    first commit

---
+++ src/main/java/com/example/bodim/house_back_end/BodimHouseBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.bodim.house_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodimHouseBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodimHouseBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bodim/house_back_end/BodimHouseBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bodim.house_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodimHouseBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
